6df961215025e041835fd8e922e8cf8ba7ee7e45
Alfredo Vanegas <alfredo@pop-os.localdomain>
2021-02-09 21:02:50 -0600

    Matriz de arriba hacia abajo funciona

--- app.js
+++ app.js
@@ -1,58 +1,93 @@
 //Recoge los datos de la página web y los regresa en una matriz
 const hacer_matriz = () => {
-    let matriz = [];
-    return matriz;
-}
+  let matriz = [];
+  return matriz;
+};
 
 /*
-* La matriz será en forma de un array multidimensional:
-* let matriz = [
-*   [1, 2, 3, 20]  -> Primer fila de números
-*   [4, 5, 3, 19]  -> Segunda fila de números 
-*   [1, 2, 3, 18]  -> n fila de números 
-* ] 
-*/
+ * La matriz será en forma de un array multidimensional:
+ * let matriz = [
+ *   [1, 2, 3, 20]  -> Primer fila de números
+ *   [4, 5, 3, 19]  -> Segunda fila de números
+ *   [1, 2, 3, 18]  -> n fila de números
+ * ]
+ */
 
 const procesarMatrizArribaAbajo = (matriz) => {
-    //Esta función se encarga de procesar la matriz y repetir operaciones
-    //Dependiendo de la cantidad de filas
-    for(let i = 0; i < matriz.length; i++){
-        let columna = conseguirColumna(matriz, i);
-        let número_escalonado = matriz[i][i];
-        if(número_escalonado === 1){
-
-        }
-        else{
-            i--;
-        }
+  //Esta función se encarga de procesar la matriz y repetir operaciones
+  //Dependiendo de la cantidad de filas
+  for (let i = 0; i < matriz.length; i++) {
+    let columna = conseguirColumna(matriz, i);
+    let número_escalonado = matriz[i][i];
+    if (número_escalonado === 1) {
+      for (let j = i + 1; j < columna.length; j++) {
+        let filaEscalonada = [];
+        if (columna[j] > 0)
+          filaEscalonada = modificarFila("multiplicar", columna[j] * -1, matriz[i]);
+        else 
+          filaEscalonada = modificarFila("multiplicar", columna[j], matriz[i]);
+        
+        matriz[j] = sumarFilas(filaEscalonada, matriz[j]);
+        console.log(matriz);
+      }
+    } else {
+      matriz[i] = modificarFila("dividir", número_escalonado, matriz[i]);
+      i--;
     }
+  }
 
-    return matriz;
-}
+  return matriz;
+};
 
-const procesarMatrizAbajoArriba = (matriz) => {
-
-}
+const procesarMatrizAbajoArriba = (matriz) => {};
 
 const conseguirColumna = (matriz, número_columna) => {
-    let columna = [];
-    for(let i = 0; i < matriz.length; i++){
-        let fila = matriz[i];
-        columna.push(fila[número_columna]);
+  let columna = [];
+  for (let i = 0; i < matriz.length; i++) {
+    let fila = matriz[i];
+    columna.push(fila[número_columna]);
+  }
+  return columna;
+};
+
+const modificarFila = (operación, número, fila) => {
+  let filaModificada = fila.map((filaNúmero) => {
+    switch (operación) {
+      case "multiplicar":
+        return filaNúmero * número;
+        break;
+      case "dividir":
+        return filaNúmero / número;
+        break;
+      default:
+        console.log("Error al pasar la función");
+        break;
     }
-    return columna;
-}
+  });
 
+  return filaModificada;
+};
 
-const registrarPasos = () => {
-}
+const sumarFilas = (primeraFila, segundaFila) => {
+  let sumaDeFilas = [];
+  for (let i = 0; i < primeraFila.length; i++)
+    sumaDeFilas.push(primeraFila[i] + segundaFila[i]);
 
-const test2 = () => {
-    //Matriz de: https://www.superprof.es/apuntes/escolar/matematicas/algebralineal/sistemas/metodo-de-gauss.html
-    let matriz_inicial = [[3,2,1,1], [5,3,4,2], [1,1,-1,1]];
-    let matriz_final = procesar_matriz(matriz_inicial);
-}
+  return sumaDeFilas;
+};
+
+const registrarPasos = () => {};
+
+const test = () => {
+  //Matriz de: https://www.superprof.es/apuntes/escolar/matematicas/algebralineal/sistemas/metodo-de-gauss.html
+  let matriz_inicial = [
+    [3, 2, 1, 1],
+    [5, 3, 4, 2],
+    [1, 1, -1, 1],
+  ];
+  let matriz_final = procesarMatrizArribaAbajo(matriz_inicial);
+};
 
 window.onload = function () {
-    test2();
-} 
+  test();
+};
